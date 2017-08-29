b1710aa745c837f492b8ab0c123e512fd8f97ae6
Chenghai XU <xuchenghai@gmail.com>
2017-08-29 15:32:22 +0800

    configure to use mysql.

--- mccloud/settings.py
+++ mccloud/settings.py
@@ -78,9 +78,12 @@ WSGI_APPLICATION = 'mccloud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mccloud',
+        'USER': 'xuch',
+        'PASSWORD': 'fi3ohkiw',
+        "HOST": "localhost",
+    },
 }
 
 
