2b00dd33db87735a4238a688fcf5e7e34d214b7b
Chenghai XU <xuchenghai@gmail.com>
2017-08-30 08:04:52 +0800

    add scheme of app mc

--- mc/models.py
+++ mc/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class MCProject(models.Model):
+    name = models.CharField(max_length=32)
+    owner = models.ForeignKey(users)
+    model = models.CharField(max_length=32)
+    material = models.CharField(max_length=32)
+    physics = models.CharField(max_length=32)
+    primary = models.CharField(max_length=32)
+
