337cacf421160c0955137759197e18d71d10d8db
Chenghai XU <xuchenghai@gmail.com>
2017-09-15 14:31:34 +0800

    Add source angular config function.

--- mc/static/mc/js/primary.js
+++ mc/static/mc/js/primary.js
@@ -161,6 +161,7 @@ function NewPositionNode(t){
 
 function NewDirectionNode(t){
     var node=NewNode(t,'direction');
+    node.data=NewAngular();
     return node;
 }
 
@@ -243,6 +244,11 @@ function SelectedDirection(current)
 {
     if(current.type != 'direction')
         return;
+    var property = $('#property-angular').clone();
+    property.attr("id","property-current");
+    property.removeClass('hidden');
+    InitAngularForm(property,current);
+    $('#property-container').append(property);
 }
 
 function SelectedEnergy(current)
@@ -710,3 +716,377 @@ function PositionAUnitChanged(elem)
     var val=$(elem).val().trim();
     current.data.aunit=val;
 }
+
+function NewAngularBase(t)
+{
+    var node={type:t};
+    node.lunit='cm';
+    node.aunit='deg';
+    return node;
+}
+
+function NewAngularIso()
+{
+    var node = NewAngularBase('Iso');
+    node.mintheta=0;
+    node.maxtheta=180;
+    node.minphi=0;
+    node.maxphi=360;
+    node.rot1={x:1,y:0,z:0};
+    node.rot2={x:0,y:1,z:0};
+    return node;
+}
+
+function NewAngularCos()
+{
+    var node = NewAngularBase('Cos');
+    node.mintheta=0;
+    node.maxtheta=90;
+    node.minphi=0;
+    node.maxphi=360;
+    node.rot1={x:1,y:0,z:0};
+    node.rot2={x:0,y:1,z:0};
+    return node;
+}
+
+function NewAngularBeam1d()
+{
+    var node = NewAngularBase('Beam1d');
+    node.sigma_r=0;
+    node.rot1={x:1,y:0,z:0};
+    node.rot2={x:0,y:1,z:0};
+    return node;
+}
+
+function NewAngularBeam2d()
+{
+    var node = NewAngularBase('Beam2d');
+    node.sigma_x=0;
+    node.sigma_y=0;
+    node.rot1={x:1,y:0,z:0};
+    node.rot2={x:0,y:1,z:0};
+    return node;
+}
+
+function NewAngularFocused()
+{
+    var node = NewAngularBase('Focused');
+    node.focuspoint={x:0,y:0,z:0};
+    return node;
+}
+
+function NewAngularPlanar()
+{
+    var node = NewAngularBase('Planar');
+    node.direction={x:0,y:0,z:1};
+    return node;
+}
+
+function NewAngular(type)
+{
+    if(type=='Iso')
+        return NewAngularIso();
+    else if(type=='Cos')
+        return NewAngularCos();
+    else if(type=='Beam1d')
+        return NewAngularBeam1d();
+    else if(type=='Beam2d')
+        return NewAngularBeam2d();
+    else if(type=='Planar')
+        return NewAngularPlanar();
+    else if(type=='Focused')
+        return NewAngularFocused();
+    return NewAngularPlanar();
+}
+
+function InitAngularForm(form,current)
+{
+    console.log('Init angular form');
+    $(form).find('div .input-group').addClass('hidden');
+    var data=current.data;
+    $(form).find('#gps-ang-type').removeClass('hidden');
+    $(form).find('select[name=type]').val(data.type);
+    if(current.data.type==='Planar')
+    {
+        InitAngularPlanarForm(form,current);
+    }
+    else if(current.data.type==='Focused')
+    {
+        InitAngularFocusedForm(form,current);
+    }
+    else if(current.data.type==='Iso' || current.data.type==='Cos')
+    {
+        InitAngularIsoForm(form,current);
+    }
+    else if(current.data.type==='Beam1d')
+    {
+        InitAngularBeam1dForm(form,current);
+    }
+    else if(current.data.type==='Beam2d')
+    {
+        InitAngularBeam2dForm(form,current);
+    }
+}
+
+function InitAngularPlanarForm(form,current)
+{
+    var data=current.data;
+    $(form).find('input[name=direction]').val(data.direction.x+', ' + data.direction.y+', ' +data.direction.z);
+    $(form).find('#gps-ang-direction').removeClass('hidden');
+}
+
+function InitAngularFocusedForm(form,current)
+{
+    var data=current.data;
+    $(form).find('select[name=lunit]').val(data.lunit);
+    $(form).find('#gps-ang-lunit').removeClass('hidden');
+    $(form).find('input[name=focuspoint]').val(data.focuspoint.x+', '+data.focuspoint.y+', '+data.focuspoint.z);
+    $(form).find('#gps-ang-focuspoint').removeClass('hidden');
+}
+
+function InitAngularIsoForm(form,current)
+{
+    var data=current.data;
+    $(form).find('select[name=aunit]').val(data.aunit);
+    $(form).find('#gps-ang-aunit').removeClass('hidden');
+
+    $(form).find('#gps-ang-rot1').removeClass('hidden');
+    $(form).find('input[name=rot1]').val(data.rot1.x+', ' + data.rot1.y + ', ' + data.rot1.z);
+    $(form).find('#gps-ang-rot2').removeClass('hidden');
+    $(form).find('input[name=rot2]').val(data.rot2.x+', ' + data.rot2.y + ', ' + data.rot2.z);
+
+    $(form).find('#gps-ang-mintheta').removeClass('hidden');
+    $(form).find('input[name=mintheta]').val(data.mintheta);
+
+    $(form).find('#gps-ang-maxtheta').removeClass('hidden');
+    $(form).find('input[name=maxtheta]').val(data.maxtheta);
+
+    $(form).find('#gps-ang-minphi').removeClass('hidden');
+    $(form).find('input[name=minphi]').val(data.minphi);
+
+    $(form).find('#gps-ang-maxphi').removeClass('hidden');
+    $(form).find('input[name=maxphi]').val(data.maxphi);
+}
+
+function InitAngularBeam1dForm(form,current)
+{
+    var data=current.data;
+    $(form).find('select[name=aunit]').val(data.aunit);
+    $(form).find('#gps-ang-aunit').removeClass('hidden');
+
+    $(form).find('#gps-ang-rot1').removeClass('hidden');
+    $(form).find('input[name=rot1]').val(data.rot1.x+', ' + data.rot1.y + ', ' + data.rot1.z);
+    $(form).find('#gps-ang-rot2').removeClass('hidden');
+    $(form).find('input[name=rot2]').val(data.rot2.x+', ' + data.rot2.y + ', ' + data.rot2.z);
+
+    $(form).find('#gps-ang-sigma-radius').removeClass('hidden');
+    $(form).find('input[name=sigma-radius]').val(data.sigma_r);
+}
+
+function InitAngularBeam2dForm(form,current)
+{
+    var data=current.data;
+    $(form).find('select[name=aunit]').val(data.aunit);
+    $(form).find('#gps-ang-aunit').removeClass('hidden');
+
+    $(form).find('#gps-ang-rot1').removeClass('hidden');
+    $(form).find('input[name=rot1]').val(data.rot1.x+', ' + data.rot1.y + ', ' + data.rot1.z);
+    $(form).find('#gps-ang-rot2').removeClass('hidden');
+    $(form).find('input[name=rot2]').val(data.rot2.x+', ' + data.rot2.y + ', ' + data.rot2.z);
+
+    $(form).find('#gps-ang-sigma-x').removeClass('hidden');
+    $(form).find('input[name=sigma-x]').val(data.sigma_x);
+
+    $(form).find('#gps-ang-sigma-y').removeClass('hidden');
+    $(form).find('input[name=sigma-y]').val(data.sigma_y);
+}
+
+function ChangeAngularType(sel){
+    var selected=$(sel).val();
+    var instance = $('#project-view').jstree(true);
+    var selects=instance.get_selected(true);
+    if(selects.length < 1)
+        return;
+    var current=selects[0];
+    if(current.type != 'direction')
+        return;
+
+    var form=$('#property-current');
+    console.log('Change position type to: ',selected);
+    current.data=NewAngular(selected);
+    InitAngularForm(form,current);
+} 
+
+function AngularDirectionChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    var val = val.split(",");
+    if(val.length!=3)
+    {
+        alert('Invalid direction value, valid format:\nx,y,z');
+    }
+    if(val[2]==='')
+    {
+        alert('Invalid direction value, valid format:\nx,y,z');
+    }
+    current.data.direction.x=parseFloat(val[0]);
+    current.data.direction.y=parseFloat(val[1]);
+    current.data.direction.z=parseFloat(val[2]);
+}
+
+function AngularRot1Changed(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    var val = val.split(",");
+    if(val.length!=3)
+    {
+        alert('Invalid rotation value, valid format:\nx,y,z');
+    }
+    if(val[2]==='')
+    {
+        alert('Invalid rotation value, valid format:\nx,y,z');
+    }
+    current.data.rot1.x=parseFloat(val[0]);
+    current.data.rot1.y=parseFloat(val[1]);
+    current.data.rot1.z=parseFloat(val[2]);
+}
+
+function AngularRot2Changed(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    var val = val.split(",");
+    if(val.length!=3)
+    {
+        alert('Invalid rotation value, valid format:\nx,y,z');
+    }
+    if(val[2]==='')
+    {
+        alert('Invalid rotation value, valid format:\nx,y,z');
+    }
+    current.data.rot2.x=parseFloat(val[0]);
+    current.data.rot2.y=parseFloat(val[1]);
+    current.data.rot2.z=parseFloat(val[2]);
+}
+
+function AngularMinThetaChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    current.data.mintheta=parseFloat(val);
+}
+
+function AngularMaxThetaChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    current.data.maxtheta=parseFloat(val);
+}
+
+function AngularMinPhiChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    current.data.minphi=parseFloat(val);
+}
+
+function AngularMaxPhiChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    current.data.maxphi=parseFloat(val);
+}
+
+function AngularSigmaRChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    current.data.sigma_r=parseFloat(val);
+}
+
+function AngularSigmaXChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    current.data.sigma_x=parseFloat(val);
+}
+
+function AngularSigmaYChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    current.data.sigma_y=parseFloat(val);
+}
+
+function AngularFocusPointChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    var val = val.split(",");
+    if(val.length!=3)
+    {
+        alert('Invalid position value, valid format:\nx,y,z');
+    }
+    if(val[2]==='')
+    {
+        alert('Invalid position value, valid format:\nx,y,z');
+    }
+    current.data.focuspoint.x=parseFloat(val[0]);
+    current.data.focuspoint.y=parseFloat(val[1]);
+    current.data.focuspoint.z=parseFloat(val[2]);
+}
+
+function AngularLUnitChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    current.data.lunit=val;
+}
+
+function AngularAUnitChanged(elem)
+{
+    var current=GetCurrentAngular();
+    if(!current)
+        return;
+    var val=$(elem).val().trim();
+    current.data.aunit=val;
+}
+
+function GetCurrentAngular()
+{
+    var instance = $('#project-view').jstree(true);
+    var selects=instance.get_selected(true);
+    if(selects.length < 1)
+        return false;
+    var current=selects[0];
+    if(current.type != 'direction')
+        return false;
+    return current;
+}
+
