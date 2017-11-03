1dbda80729704b9bb428420411c159b9e1233967
Chenghai XU <xuchenghai@gmail.com>
2017-11-03 18:03:03 +0800

    Add require login for certain page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import url
+from django.http import JsonResponse, HttpResponseRedirect, Http404
+
+def require_login(view):
+    def new_view(request,*args,**kwargs):
+        if not request.user.is_authenticated():
+            return HttpResponseRedirect('/accounts/login/')
+        return view(request,*args,**kwargs)
+    return new_view
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', require_login(views.home), name='home'),
 ]
+
--- mc/urls.py
+++ mc/urls.py
@@ -1,18 +1,27 @@
 from django.conf.urls import url
 
+from django.http import JsonResponse, HttpResponseRedirect, Http404
+
+def require_login(view):
+    def new_view(request,*args,**kwargs):
+        if not request.user.is_authenticated():
+            return HttpResponseRedirect('/accounts/login/')
+        return view(request,*args,**kwargs)
+    return new_view
+
 from . import views
 from . import project
 from . import job
 from . import material
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^api/project/', views.ProjectView.as_view(), name='ProjectView'),
-    url(r'^api/logical/', views.LogicalView.as_view(), name='LogicalView'),
-    url(r'^api/physical/', views.PhysicalView.as_view(), name='PhysicalView'),
-    url(r'^api/solid/', views.SolidView.as_view(), name='SolidView'),
-    url(r'^project-tree/', project.ProjectView.as_view(), name='Project-Tree'),
-    url(r'^job/', job.JobView.as_view(), name='JobView'),
-    url(r'^material/', material.MaterialView.as_view(), name='MaterialView'),
+    url(r'^$', require_login(views.index), name='index'),
+    url(r'^api/project/', require_login(views.ProjectView.as_view()), name='ProjectView'),
+    url(r'^api/logical/', require_login(views.LogicalView.as_view()), name='LogicalView'),
+    url(r'^api/physical/', require_login(views.PhysicalView.as_view()), name='PhysicalView'),
+    url(r'^api/solid/', require_login(views.SolidView.as_view()), name='SolidView'),
+    url(r'^project-tree/', require_login(project.ProjectView.as_view()), name='Project-Tree'),
+    url(r'^job/', require_login(job.JobView.as_view()), name='JobView'),
+    url(r'^material/', require_login(material.MaterialView.as_view()), name='MaterialView'),
 
 ]
