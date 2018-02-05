b2f719c4319b6e751a4bfce87ae08485681581e8
Chenghai XU <xuchenghai@gmail.com>
2018-02-05 16:02:24 +0800

    Switch to redis

--- mccloud/settings.py
+++ mccloud/settings.py
@@ -148,7 +148,8 @@ STATICFILES_FINDERS = (
 LOGIN_REDIRECT_URL = '/mc'
 
 #Celery Config
-BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+#BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+BROKER_URL = 'redis://localhost:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
