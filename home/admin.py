73fc56cede68325863200919e57efac466347544
XU Chenghai <xuchd@dchealth.com>
2018-03-21 05:40:40 +0000

    Add execute charge in admin site.

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,35 @@
 from django.contrib import admin
 from .models import *
 
+def execute_charge(modeladmin, request, queryset):
+    for item in queryset:
+        if item.executed:
+            continue
+        user=item.user;
+        try:
+            cash=Cash.objects.get(user=user)
+        except:
+            cash=Cash.objects.get(user=user)
+        item.executed=True
+        cash.value+=item.value
+        cash.save()
+        item.save()
+execute_charge.short_description="Add charge value to cash"
+
+def undo_execute_charge(modeladmin, request, queryset):
+    for item in queryset:
+        if item.executed==False:
+            continue
+        user=item.user;
+        try:
+            cash=Cash.objects.get(user=user)
+        except:
+            cash=Cash.objects.get(user=user)
+        item.executed=False
+        cash.value-=item.value
+        cash.save()
+        item.save()
+undo_execute_charge.short_description="Decrease charge value from cash"
 # Register your models here.
 @admin.register(Cash)
 class CashAdmin(admin.ModelAdmin):
@@ -9,3 +38,5 @@ class CashAdmin(admin.ModelAdmin):
 @admin.register(Charge)
 class ChargeAdmin(admin.ModelAdmin):
     list_display = ('id', 'user', 'value','executed','create_time')
+    actions = [execute_charge,undo_execute_charge]
+    list_filter = ('executed', 'user')
