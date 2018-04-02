b3d0411f90a62c00e91d87169ce8de7614461fc9
XU Chenghai <xuchd@dchealth.com>
2018-04-02 07:01:02 +0000

    Fine tune

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python manage.py collectstatic
+python manage.py collectstatic --noinput
 #sudo systemctl restart supervisor
 sudo supervisorctl restart celery
 sudo systemctl restart apache2.service
