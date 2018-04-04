7fdd8dd078a15cbf1e5af8a50abb8b3d6c5d3fb4
xuch <xuchenghai@gmail.com>
2018-04-04 03:35:15 +0000

    restart postgresql

--- restart.sh
+++ restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 python manage.py collectstatic --noinput
-#sudo systemctl restart supervisor
+sudo systemctl restart postgresql
+sudo systemctl restart supervisor
 sudo supervisorctl restart celery
 sudo systemctl restart apache2.service
