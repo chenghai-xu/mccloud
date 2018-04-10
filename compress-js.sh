61e7e96559dda10e87a1b880c91348470b3a7f08
XU Chenghai <xuchenghai@gmail.com>
2018-04-10 04:30:13 +0000

    Add compress-js

---
+++ compress-js.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#https://developers.google.com/closure/compiler/?csw=1
+#https://stackoverflow.com/questions/194397/how-can-i-obfuscate-protect-javascript
+
+files=$(find collected_static/mc collected_static/home | grep '\.js')
+for file in $files
+do
+   echo $file
+   java -jar closure-compiler/closure-compiler.jar --js "$file" --js_output_file "$file.tmp" 
+   mv "$file.tmp" "$file"
+done
